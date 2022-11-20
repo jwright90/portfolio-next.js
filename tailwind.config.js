28ee913b8cc7462ce250eb114d96516182cd051b
jwright90 <jason.wright90@gmail.com>
2022-11-20 19:20:01 +0000

    update tailwind theme and begin landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +7,28 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      dark: {
+        100: '#242424',
+        200: '#151515'
+      },
+      light: {
+        100: '#FFFFFF',
+        200: '#D9D9D9'
+      },
+      green: '#4EE1A0'
+    },
+    fontSize: {
+      'base': ['18px', { lineHeight: '24px', fontWeight: '500' }],
+      'm': ['24px', { lineHeight: '32px', fontWeight: '700' }],
+      'l': ['48px', {lineHeight: '56px', letterSpacing: '-1.5px', fontWeight: '700'}],
+      'xl': ['88px', { lineHeight: '88px', letterSpacing: '-2.5px', fontWeight: '700' }]
+    },
+    extend: {
+      fontFamily: {
+        space: ['"Space Grotesk"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
