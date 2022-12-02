12f879f5cb56d3a076dd8d48833ede020d6e485d
jwright90 <jason.wright90@gmail.com>
2022-12-02 18:26:55 +0000

    fix overflow issue on mobile devices

--- components/Hero.js
+++ components/Hero.js
@@ -18,23 +18,26 @@ const Hero = () => {
         </div>
 
         <p className="sm:text-[21px] text-light-200 text-base md:w-[400px] xl:w-[580px]">
-          Based in Manchester, I&apos;m seeking my first frontend developer position, as I pursue a career change from management accounting.
-          I&apos;ve developed a broad skillset coding as a hobby over the years using various languages and frameworks.
+          I&apos;m seeking my first frontend developer position, pursuing a career change from management accounting. <br /><br />
+
+          I&apos;ve developed a broad skillset coding as a hobby over the years using various languages and frameworks, both front and backend.
+          My preference is Next.js, but I&apos;m open to PHP based roles or learning a new stack.
         </p>
 
         <button className="uppercase mt-12 py-4 text-base tracking-widest border-b-2 border-green hover:text-green hover:cursor-pointer">
           Contact Me
         </button>
+
       </div>
 
 
-      <div className="bg-gradient-to-t from-dark-100 md:bg-dark-100 md:mt-[55px] min-h-[300px] sm:min-h-[450px] md:min-h-[720px] col-span-6 col-start-4 md:col-span-5 md:col-start-8 lg:col-start-7 row-end-1 row-span-1 md:row-span-full z-20 relative">
+      <div className="relative bg-gradient-to-t from-dark-100 md:bg-dark-100 md:mt-[55px] min-h-[300px] sm:min-h-[450px] md:min-h-[720px] col-span-6 col-start-4 md:col-span-5 md:col-start-8 lg:col-start-7 row-end-1 row-span-1 md:row-span-full z-20 ">
         <Image
           src='/images/jason-profile-bw.png'
           fill
           style={{ 'pointerEvents': 'none', 'userSelect': 'none', 'objectFit': 'cover' }}
         />
-        <div className="h-[120px] w-[120px] md:h-[80px] md:w-[80px] lg:h-[100px] lg:w-[100px] 2xl:h-[120px] 2xl:w-[120px] absolute bottom-[20px] left-[300px] sm:left-[425px] md:left-[250px] lg:left-[380px] xl:left-[480px] 2xl:left-[-50px]">
+        <div className="h-[110px] w-[110px] md:h-[80px] md:w-[80px] lg:h-[100px] lg:w-[100px] 2xl:h-[120px] 2xl:w-[120px] absolute top-[225px] sm:top-[550px] left-[225px] sm:left-[425px] md:left-[250px] lg:left-[380px] xl:left-[480px] 2xl:left-[-50px]">
           <Image
             src='/images/pattern-circle.svg'
             fill
--- components/layout/Wrapper.js
+++ components/layout/Wrapper.js
@@ -1,7 +1,7 @@
 
 function Wrapper({ children, bg, additionalCss }) {
   return (
-    <div className={`${bg} ${additionalCss} font-space text-light-100 selection:bg-[#737996] text-center md:text-left`}>
+    <div className={`${bg} ${additionalCss} font-space text-light-100 selection:bg-[#737996] text-center md:text-left overflow-hidden`}>
       <div className="mx-auto container px-8 lg:px-0">
         {children}
       </div>
